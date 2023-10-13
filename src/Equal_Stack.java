e5457a1c55956ed58285ba27d493553ffde59de1
saifulislam05 <saiful.bijulia@gmail.com>
2023-10-13 19:13:22 +0530

    added _7Min_add_to_make_parentheses_valid

---
+++ src/Equal_Stack.java
@@ -0,0 +1,32 @@
+
+public class Equal_Stack {
+    
+}
+
+// -----------BruteForce Approach------------
+
+//     public static int calcSum(List<Integer> list){
+//         int sum=0;
+//         for(int ele:list){
+//             sum+=ele;
+//         }
+//         return sum;
+//     }
+//    public static int equalStacks(List<Integer> h1, List<Integer> h2, List<Integer> h3) {
+//     int stack1=calcSum(h1);
+//     int stack2=calcSum(h2);
+//     int stack3=calcSum(h3);
+//     while(!(s1==s2) && (s2==s3)){
+//         if(s1 >= s2 && s1 >= s3){
+//             s1.remove(0);
+//         }
+//         else if(s2 >= s1 && s2 >= s3){
+//             s2.remove(0);
+//         }
+//         else if(s3 >= s1 && s3 >= s2){
+//             s3.remove(0);
+//         }
+//     }
+
+//     return s1
+//    }
---
+++ src/_7Min_add_to_make_parentheses_valid.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class _7Min_add_to_make_parentheses_valid {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        scn.close();
+
+        int openP = 0;
+        int closeP = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') {
+                openP++;
+            } else if (openP != 0 && str.charAt(i) == ')') {
+                openP--;
+            } else {
+                closeP++;
+            }
+        }
+        
+        int ans = openP + closeP;
+        System.out.print(ans);
+    }
+}
