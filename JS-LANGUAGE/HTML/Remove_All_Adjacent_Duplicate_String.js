5dfa83df13aef3d26f73d62bb6ce13fe02aeb9e8
saifulislam05 <saiful.bijulia@gmail.com>
2023-10-18 10:34:46 +0530

    added Remove All Adjacent Duplicate from String in js

---
+++ JS-LANGUAGE/HTML/Remove_All_Adjacent_Duplicate_String.js
@@ -0,0 +1,17 @@
+function removeDuplicates(s) {
+         let stackArr=[];
+    for(let i=0;i<s.length;i++){
+        let ch=s[i];
+        let top=stackArr[stackArr.length-1];
+        if(ch==top){
+            stackArr.pop();
+        }else{
+            stackArr.push(ch);
+        }
+    }
+    return stackArr.join('');
+}
+
+console.log(removeDuplicates("abbacdccd"));
+   
+    
