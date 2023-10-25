3a943e6e75d786bfa7d8b3437e1c04c9e068cacb
saifulislam05 <saiful.bijulia@gmail.com>
2023-10-25 06:43:01 +0530

    added Contains Duplicate in array in java

---
+++ src/Contains_Duplicate.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Contains_Duplicate {
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hs=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            hs.add(nums[i]);
+        }
+        return nums.length != hs.size();
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        scn.close();
+        boolean ans = containsDuplicate(arr);
+       System.out.println(ans);
+    }
+}
